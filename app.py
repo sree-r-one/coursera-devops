5687a64d3c96c3f2206764f01f06443baaed068b
sree-r-one <sreeraj_ec@hotmail.com>
2023-10-02 08:13:51 +0800

    added commits to master

--- app.py
+++ app.py
@@ -1,4 +1,66 @@
-from flask import Flask
+# region IMPORT
+from flask import Flask, make_response, request
+
+# endregion IMPORT
+
+
+data = [
+    {
+        "id": "3b58aade-8415-49dd-88db-8d7bce14932a",
+        "first_name": "Tanya",
+        "last_name": "Slad",
+        "graduation_year": 1996,
+        "address": "043 Heath Hill",
+        "city": "Dayton",
+        "zip": "45426",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/139x100.png/cc0000/ffffff",
+    },
+    {
+        "id": "d64efd92-ca8e-40da-b234-47e6403eb167",
+        "first_name": "Ferdy",
+        "last_name": "Garrow",
+        "graduation_year": 1970,
+        "address": "10 Wayridge Terrace",
+        "city": "North Little Rock",
+        "zip": "72199",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/148x100.png/dddddd/000000",
+    },
+    {
+        "id": "66c09925-589a-43b6-9a5d-d1601cf53287",
+        "first_name": "Lilla",
+        "last_name": "Aupol",
+        "graduation_year": 1985,
+        "address": "637 Carey Pass",
+        "city": "Gainesville",
+        "zip": "32627",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/174x100.png/ff4444/ffffff",
+    },
+    {
+        "id": "0dd63e57-0b5f-44bc-94ae-5c1b4947cb49",
+        "first_name": "Abdel",
+        "last_name": "Duke",
+        "graduation_year": 1995,
+        "address": "2 Lake View Point",
+        "city": "Shreveport",
+        "zip": "71105",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/145x100.png/dddddd/000000",
+    },
+    {
+        "id": "a3d8adba-4c20-495f-b4c4-f7de8b9cfb15",
+        "first_name": "Corby",
+        "last_name": "Tettley",
+        "graduation_year": 1984,
+        "address": "90329 Amoth Drive",
+        "city": "Boulder",
+        "zip": "80305",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/198x100.png/cc0000/ffffff",
+    },
+]
 
 app = Flask(__name__)
 
@@ -6,3 +68,73 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, World!"
+
+
+# For No Content Response
+@app.route("/no_content")
+def no_content():
+    """
+    return No Content response with Status code 204
+    """
+    return ({"message": "no content found"}, 204)
+
+
+@app.route("/exp")
+def inde_explicit():
+    """
+    Return hello world with explicit response with status code 200
+    """
+    resp = make_response({"message": "Hello world"})
+    resp.status_code = 200
+    return resp
+
+
+@app.route("/data")
+def get_data():
+    """Get data from the server"""
+    try:
+        if data and len(data) > 0:
+            return ({"data": data}, 200)
+        return ({"message": "no content found"}, 204)
+    except Exception as e:
+        return ({"message": str(e)}, 500)
+
+
+@app.route("/name_search")
+def name_search():
+    """Search the name in the data"""
+
+    query_params = request.args.get("q")
+
+    if not query_params:
+        return ({"message": "query param not found"}, 422)
+
+    for person in data:
+        if query_params.lower() in person.get("first_name").lower():
+            return person, 400
+
+    return ({"message": "query param not found"}, 404)
+
+
+@app.route("/count")
+def get_count():
+    """Get the count of the data"""
+    try:
+        return ({"count": len(data)}, 200)
+    except Exception as e:
+        return ({"message": str(e)}, 500)
+
+
+@app.route("/person/<string:uuid>")
+def find_by_uuid(uuid):
+    """Find the person by uuid"""
+
+    for person in data:
+        if person.get("id") == uuid:
+            return person, 200
+
+    return ({"message": "query param not found"}, 404)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
